1b16edf6f5ca16aab097274c6087d3b537e2cb28
mikeymop <michael@defrances.co>
2024-07-11 23:05:04 -0500

    Refine behavior of the navbar

--- web/src/components/ui/navbar.tsx
+++ web/src/components/ui/navbar.tsx
@@ -1,59 +1,38 @@
 import { Aperture } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { cn } from "@/lib/utils";
 
-/** Return tw bold font if path matches current route. */
-const isActive = (path: string) =>
-  path === window.location.pathname ? "font-semibold" : "text-foreground";
+/** Renders a Navbar button. */
+const NavLink = ({ to, title }: { to: string; title: string }) => {
+  const { pathname } = useLocation();
+  return (
+    <Link
+      to={to}
+      className={cn(
+        "transition-colors hover:text-foreground",
+        pathname === to ? "font-semibold" : "text-foreground",
+      )}
+    >
+      {title}
+    </Link>
+  );
+};
 
 /** Isotope Navbar
  * @see https://lucide.dev/icons/
  */
 const Nav = (): JSX.Element => {
-  console.log("Active: ", window.location.pathname, isActive("/gallery"));
   return (
     <header className="sticky top-0 flex h-12 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link to="/" className="flex items-center gap-2 text-lg md:text-base">
           <Aperture className="h-6 w-6" />
-          <span className="sr-only">Acme Inc</span>
+          <span className="sr-only">Isotope</span>
         </Link>
-        <Link
-          to="/"
-          className={cn(
-            "transition-colors hover:text-foreground",
-            isActive("/"),
-          )}
-        >
-          Dashboard
-        </Link>
-        <Link
-          to="/gallery"
-          className={cn(
-            "transition-colors hover:text-foreground",
-            isActive("/gallery"),
-          )}
-        >
-          Galleries
-        </Link>
-        <Link
-          to="#"
-          className={cn(
-            "transition-colors hover:text-foreground",
-            isActive("/photos"),
-          )}
-        >
-          Photos
-        </Link>
-        <Link
-          to="#"
-          className={cn(
-            "transition-colors hover:text-foreground",
-            isActive("/customers"),
-          )}
-        >
-          Customers
-        </Link>
+        <NavLink to="/" title="Dashboard" />
+        <NavLink to="/gallery" title="Galleries" />
+        <NavLink to="/photos" title="Photos" />
+        <NavLink to="/customers" title="Customers" />
       </nav>
     </header>
   );
